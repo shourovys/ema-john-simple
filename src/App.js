9cfb643a84a8214bafaaf9f67d175a5cd902c2c7
shourovys <shourovys@gmail.com>
2020-03-12 13:35:32 +0600

    Logo and nav-bar complit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './componant/Header/Header';
+import Shop from './componant/Shop/Shop';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Shop></Shop>
     </div>
   );
 }
